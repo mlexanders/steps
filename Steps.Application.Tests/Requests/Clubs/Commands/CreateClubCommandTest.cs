34e320e044713d73d4b15a424f4a9a55c8574501
Eva <evapirozhok@gmail.com>
2025-02-27 23:24:20 +0300

    change tests

--- Steps.Application.Tests/Requests/Clubs/Commands/CreateClubCommandTest.cs
+++ Steps.Application.Tests/Requests/Clubs/Commands/CreateClubCommandTest.cs
@@ -49,7 +49,8 @@ public class CreateClubCommandTest
     }
     
     [Fact]
-    public async Task Create_Club_Test()
+    //Подаем корректные данные и проверяем ошибки при работе с бд
+    public async Task SqlFail()
     {
         //Arrange
         var mediator = _scope.ServiceProvider.GetRequiredService<IMediator>();
@@ -60,33 +61,91 @@ public class CreateClubCommandTest
         try
         {
             var result = await mediator.Send(command);
-            Assert.True(result.IsSuccess);
-            Assert.Null(result.Value);
         }
         catch (AppHandledException ex)
         {
-            switch (ex.InnerException)
-            {
-                case AppAccessDeniedException _:
-                    Assert.Equal(typeof(AppAccessDeniedException), ex.InnerException.GetType());
-                    return;
+            Assert.IsType<PostgresException>(ex.InnerException);
+        }
+    }
+    
+    [Fact]
+    //Подаем корректные данные и проверяем успешное создание
+    public async Task AllRight()
+    {
+        //Arrange
+        var mediator = _scope.ServiceProvider.GetRequiredService<IMediator>();
+        var createClubModel = new CreateClubViewModel { Name = "Test Club", OwnerId = Guid.NewGuid() };
+        var command = new CreateClubCommand(createClubModel);
+        
+        //Act
+        try
+        {
+            var result = await mediator.Send(command);
+        }
+        //Assert
+        catch (AppHandledException ex)
+        {
+           Assert.Null(ex.Result.Errors);
+        }
+    }
 
-                case StepsBusinessException _:
-                    Assert.Equal(typeof(StepsBusinessException), ex.InnerException.GetType());
-                    return;
+    [Fact]
+    //Подаем юзера, которому доступ на создание запрещен
+    public async Task NotAuthorized()
+    {
+        //Arrange
+        var mediator = _scope.ServiceProvider.GetRequiredService<IMediator>();
+        var createClubModel = new CreateClubViewModel { Name = "Test Club", OwnerId = Guid.NewGuid() };
+        var command = new CreateClubCommand(createClubModel);
+        
+        //Act
+        try
+        {
+            var result = await mediator.Send(command);
+        }
+        catch (AppHandledException ex)
+        {
+            Assert.IsType<AppAccessDeniedException>(ex.InnerException);
+        }
+    }
 
-                case PostgresException _:
-                    Assert.Equal(typeof(PostgresException), ex.InnerException.GetType());
-                    return;
-                
-                case FluentValidation.ValidationException _:
-                    Assert.Equal(typeof(FluentValidation.ValidationException), ex.InnerException.GetType());
-                    return;
+    [Fact]
+    //Подаем существующий клуб
+    public async Task ClubIsExist()
+    {
+        //Arrange
+        var mediator = _scope.ServiceProvider.GetRequiredService<IMediator>();
+        var createClubModel = new CreateClubViewModel { Name = "Test Club", OwnerId = Guid.NewGuid() };
+        var command = new CreateClubCommand(createClubModel);
+        
+        //Act
+        try
+        {
+            var result = await mediator.Send(command);
+        }
+        catch (AppHandledException ex)
+        {
+            Assert.IsType<StepsBusinessException>(ex.InnerException);
+        }
+    }
 
-                default:
-                    Assert.False(ex.Result.IsSuccess);
-                    break;
-            }
+    [Fact]
+    //Подаем невалидируемые данные
+    public async Task NotValidatingData()
+    {
+        //Arrange
+        var mediator = _scope.ServiceProvider.GetRequiredService<IMediator>();
+        var createClubModel = new CreateClubViewModel { Name = "Test Club", OwnerId = Guid.NewGuid() };
+        var command = new CreateClubCommand(createClubModel);
+        
+        //Act
+        try
+        {
+            var result = await mediator.Send(command);
+        }
+        catch (AppHandledException ex)
+        {
+            Assert.IsType<FluentValidation.ValidationException>(ex.InnerException);
         }
     }
  }

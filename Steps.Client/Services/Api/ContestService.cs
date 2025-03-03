974cc5f35f25157379a3e1ca875dba66375c24d0
mlexanders <mlexanders@outlook.com>
2025-03-03 04:14:13 +0300

    fix

--- Steps.Client/Services/Api/ContestService.cs
+++ Steps.Client/Services/Api/ContestService.cs
@@ -1,12 +1,13 @@
 ﻿using Steps.Client.Services.Api.Base;
 using Steps.Client.Services.Api.Routes;
 using Steps.Domain.Entities;
+using Steps.Shared;
 using Steps.Shared.Contracts.Contests;
 using Steps.Shared.Contracts.Contests.ViewModels;
 
 namespace Steps.Client.Services.Api;
 
-public class ContestsesService : CrudService<ContestViewModel, CreateContestViewModel, UpdateContestViewModel>, IContestsService
+public class ContestsesService : CrudService<Contest, ContestViewModel, CreateContestViewModel, UpdateContestViewModel>, IContestsService
 {
     public ContestsesService(HttpClientService httpClient) : base(httpClient, new ApiRoutes.ContestsRoute())
     {
--- Steps.Shared/Contracts/Contests/IContestService.cs
+++ Steps.Shared/Contracts/Contests/IContestService.cs
@@ -3,7 +3,7 @@ using Steps.Shared.Contracts.Contests.ViewModels;
 
 namespace Steps.Shared.Contracts.Contests;
 
-public interface IContestsService : ICrudService<Contest, ContestViewModel, CreateContestViewModel, UpdateContestViewModel>;
+public interface IContestsService : ICrudService<Contest, ContestViewModel, CreateContestViewModel, UpdateContestViewModel>
 {
     Task<Result> GenerateGroupBlocks(Guid contestId, int athletesCount);
     Task<Result> CheckAthlete(Guid athleteId, Guid contestId, bool isAppeared);

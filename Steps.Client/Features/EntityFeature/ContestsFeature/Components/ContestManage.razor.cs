5d7dab7b2f41b64a58942b7aa5a0bb8f69ad7bbc
VVSaltykov <vitya.saltyckov@gmail.com>
2025-03-06 22:38:23 +0400

    add sheduler

--- Steps.Client/Features/EntityFeature/ContestsFeature/Components/ContestManage.razor.cs
+++ Steps.Client/Features/EntityFeature/ContestsFeature/Components/ContestManage.razor.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Radzen;
+using Steps.Client.Features.EntityFeature.ContestsFeature.Dialogs;
 using Steps.Client.Features.EntityFeature.ContestsFeature.Services;
 using Steps.Domain.Entities;
 using Steps.Shared.Contracts.Contests.ViewModels;
@@ -8,13 +10,14 @@ namespace Steps.Client.Features.EntityFeature.ContestsFeature.Components;
 public partial class
     ContestManage : ManageBaseComponent<Contest, ContestViewModel, CreateContestViewModel, UpdateContestViewModel>
 {
-    [Inject] protected ContestManager ClubsManager { get; set; } = null!;
+    [Inject] protected ContestManager ContestManager { get; set; } = null!;
     [Inject] protected ContestDialogManager ContestDialogManager { get; set; } = null!;
-
+    
     protected override void OnInitialized()
     {
-        Manager = ClubsManager;
+        Manager = ContestManager;
         DialogManager = ContestDialogManager;
+
         base.OnInitialized();
     }
 }

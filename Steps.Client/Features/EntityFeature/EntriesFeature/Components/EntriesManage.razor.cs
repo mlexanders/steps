2e7eb6ba8dd714789a0f7b42db7aa73fd5e57148
VVSaltykov <vitya.saltyckov@gmail.com>
2025-03-07 22:18:49 +0400

    add entries manage

--- Steps.Client/Features/EntityFeature/EntriesFeature/Components/EntriesManage.razor.cs
+++ Steps.Client/Features/EntityFeature/EntriesFeature/Components/EntriesManage.razor.cs
@@ -1,6 +1,27 @@
-﻿namespace Steps.Client.Features.EntityFeature.EntriesFeature.Components;
+﻿using Microsoft.AspNetCore.Components;
+using Steps.Client.Features.EntityFeature.EntriesFeature.Services;
+using Steps.Domain.Entities;
+using Steps.Shared.Contracts.Entries.ViewModels;
 
-public partial class EntriesManage
+namespace Steps.Client.Features.EntityFeature.EntriesFeature.Components;
+
+public partial class EntriesManage : ManageBaseComponent<Entry, EntryViewModel, CreateEntryViewModel, UpdateEntryViewModel>
 {
-    
+    [Inject] protected EntriesManagement EntriesManagement { get; set; } = null!;
+    [Inject] protected EntriesDialogManager EntriesDialogManager { get; set; } = null!;
+
+    [Parameter] public bool IsReadonly { get; set; }
+
+    protected override void OnInitialized()
+    {
+        try
+        {
+            Manager = EntriesManagement;
+            DialogManager = EntriesDialogManager;
+
+
+            base.OnInitialized();
+        }
+        catch (Exception ex) { }
+    }
 }

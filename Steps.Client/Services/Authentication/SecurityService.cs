e89f731f7ebb4eeee023e413e00446c8a9161f8e
mlexanders <mlexanders@outlook.com>
2025-03-14 20:18:18 +0300

    add refresh

--- Steps.Client/Services/Authentication/SecurityService.cs
+++ Steps.Client/Services/Authentication/SecurityService.cs
@@ -2,25 +2,45 @@
 using Steps.Domain.Base;
 using Steps.Shared;
 using Steps.Shared.Contracts.Accounts.ViewModels;
+using Timer = System.Timers.Timer;
 
 namespace Steps.Client.Services.Authentication;
 
-public class SecurityService
+public class SecurityService : IDisposable
 {
     private readonly AccountService _accountService;
     private IUser? _currentUser;
+    private readonly Timer _refreshTimer;
 
     public event Action<IUser?>? OnUserChanged;
 
     public SecurityService(AccountService accountService)
     {
         _accountService = accountService;
+       _refreshTimer = new Timer(TimeSpan.FromMinutes(1)); // TODO:
+       _refreshTimer.Elapsed += (e, v) => _ = Refresh();
+    }
+
+    private async Task Refresh()
+    {
+        var refreshedUser = await GetCurrentUserRequest();
+
+        if (refreshedUser is not null && refreshedUser.Id == _currentUser?.Id) return; 
+        
+        _currentUser = refreshedUser;
+        _refreshTimer.Stop();
+        NotifyUserChanged(_currentUser);
     }
 
     public async Task<IUser?> GetCurrentUser()
     {
         if (_currentUser is not null) return _currentUser;
 
+        return await GetCurrentUserRequest();
+    }
+
+    private async Task<IUser?> GetCurrentUserRequest()
+    {
         try
         {
             var result = await _accountService.GetCurrentUser();
@@ -28,10 +48,8 @@ public class SecurityService
         }
         catch
         {
-            /*ignore*/
+            return null;
         }
-
-        return null;
     }
 
     public async Task<Result<UserViewModel>> Login(LoginViewModel model)
@@ -41,7 +59,7 @@ public class SecurityService
 
         _currentUser = result.Value;
         NotifyUserChanged(_currentUser);
-
+        _refreshTimer.Start();
         return result;
     }
 
@@ -53,6 +71,7 @@ public class SecurityService
             throw new InvalidOperationException($"Unable to log out: {result.Message}");
         }
 
+        _refreshTimer.Stop();
         _currentUser = null;
         NotifyUserChanged(_currentUser);
     }
@@ -60,5 +79,10 @@ public class SecurityService
     private void NotifyUserChanged(IUser? resultValue)
     {
         OnUserChanged?.Invoke(resultValue);
+    }
+
+    public void Dispose()
+    {
+        _refreshTimer.Dispose();
     }
 }

660d87a9a8f35d89794c3c729423404d888dbecf
mlexanders <mlexanders@outlook.com>
2025-03-21 23:38:14 +0300

    not ready

--- Steps.Application/Requests/Ratings/CreateDiplomasCommand.cs
+++ Steps.Application/Requests/Ratings/CreateDiplomasCommand.cs
@@ -1,8 +1,12 @@
 ﻿using Calabonga.UnitOfWork;
 using MediatR;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
 using Steps.Domain.Entities;
 using Steps.Shared;
 using Steps.Shared.Contracts.Ratings;
+using Steps.Shared.Exceptions;
+using Steps.Shared.Utils;
 
 namespace Steps.Application.Requests.Ratings;
 
@@ -20,12 +24,16 @@ public class CreateDiplomasCommandHandler : IRequestHandler<CreateDiplomasComman
 
     public async Task<Result<DiplomasViewModel>> Handle(CreateDiplomasCommand request, CancellationToken cancellationToken)
     {
-        var ratingsIds = request.Ratings.Select(r => r.Id);
-
-        var ratings = await _unitOfWork.GetRepository<Rating>().GetAllAsync(
-            predicate: r => ratingsIds.Contains(r.Id)
-            && !r.IsComplete,
-            trackingType: TrackingType.Tracking);
+        var ratingsIds = request.Ratings.Select(r => r.Id).ToList();
+        if (ratingsIds.Count == 0)
+        {
+            throw new StepsBusinessException("Пустой список");
+        }
+        
+        var ratings = (await _unitOfWork.GetRepository<Rating>().GetAllAsync(
+            predicate: r => ratingsIds.Contains(r.Id),
+          
+            trackingType: TrackingType.Tracking)).ToList();
 
         foreach (var rating in ratings)
         {
@@ -34,11 +42,43 @@ public class CreateDiplomasCommandHandler : IRequestHandler<CreateDiplomasComman
 
         await _unitOfWork.SaveChangesAsync();
 
+        var prefix = "ratings.First()?.CertificateDegree.GetDisplayName();";
         var view = new DiplomasViewModel()
         {
-            Url = ""
+            FileName = $"{prefix}_{DateTime.UtcNow.AddHours(3):T}.pdf",
+            FileBytes = CreatePdf(ratings)
         };
         
         return Result<DiplomasViewModel>.Ok(view);
     }
+    
+    public static byte[] CreatePdf(List<Rating> scores)
+    {
+        using var document = new PdfDocument();
+    
+        var font = new XFont("Times New Roman", 20);
+        var textPositions = new XPoint[]
+        {
+            new XPoint(501f, 766f),
+            new XPoint(501f, 966f),
+            new XPoint(501f, 1166f)
+        };
+        
+        foreach (var data in scores)
+        {
+            var page = document.AddPage();
+            page.Width = XUnit.FromMillimeter(1237.0);
+            page.Height = XUnit.FromMillimeter(1750.0); 
+            var gfx = XGraphics.FromPdfPage(page);
+
+            gfx.DrawString("data.Athlete", font, XBrushes.Black, textPositions[0]);
+            gfx.DrawString("data.b", font, XBrushes.Black, textPositions[1]);
+            gfx.DrawString($"{data.TotalScore}", font, XBrushes.Black, textPositions[2]);
+        }
+
+        using var stream = new MemoryStream();
+        document.Save(stream);
+        return stream.ToArray();
+    }
+
 }
